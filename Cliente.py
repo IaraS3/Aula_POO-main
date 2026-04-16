aff270a94e903baad687151a400b8712f3db8f4e
IaraS3 <iara6114@gmail.com>
2026-04-15 22:16:44 -0300

    att

--- Cliente.py
+++ Cliente.py
@@ -1,9 +1,6 @@
-# A evolucao de um dicionário é criação de uma classe
-# __init__ Construtor > Cria um molde dos dados do cliente, o objetivo é conseguir transferir dados por todos os arquivos python
 class Cliente:
-    def __init__(self, nome_cliente, cpf, tipo_conta, numero_conta, agencia, extrato_bancario):
-        # Atributos
-        self.nome_cliente = nome_cliente
+    def __init__(self, nome_cliente, cpf, tipo_conta, numero_conta=0, agencia = 400, extrato_bancario =0):
+        self.nome_cliente = nome_cliente      
         self.cpf = cpf
         self.tipo_conta = tipo_conta
         self.numero_conta = numero_conta
@@ -11,4 +8,8 @@ class Cliente:
         self.extrato_bancario = extrato_bancario
 
     def __str__(self):
-        return f"Nome: {self.nome_cliente} | CPF: {self.cpf} | Tipo Conta: {self.tipo_conta} | Numero Conta: {self.numero_conta} | Agencia: {self.agencia} | Extrato bancario: {self.extrato_bancario}"
+        return f"Nome: {self.nome_cliente} | CPF: {self.cpf} | Tipo Conta: {self.tipo_conta} | Numero Conta: {self.numero_conta} | Agencia: {self.agencia} | Extrato: {self.extrato_bancario}"
+
+    #Metodo
+    def dicionario_cliente(self):
+        return {chave: [valor] for chave , valor in self.__dict__.items()}  # o dict pega o objeto e transforma em dicionario
--- banco_TABAJARA.py
+++ banco_TABAJARA.py
@@ -1,46 +1,59 @@
 from Cliente import Cliente
-#    arquivo.py    o nome da nossa classe
-from Criar_conta import Criar_conta
+from criar_conta import Criar_conta
+from Adicionar_conta import Adicionar_conta
+from acessar_conta import Acessar_conta
 import pandas as pd
 import os
-
 caminho_excel = "cliente_banco_Tabajara.xlsx"
 
 print("================================================")
 print("                 BANCO TABAJARA")
-print("                                 ")
 print("                 Escolha uma opção")
 print("                 1 - Criar conta")
 print("                 2 - Acessar conta")
 print("================================================\n")
+
 opcao = int(input("R: "))
 
 if opcao == 1:
-    print("Opcao 1 selecionada")
     nome_cliente = str(input("Nome completo: "))
     cpf = int(input("CPF: "))
-    tipo_conta  = str(input("Tipo da conta que deseja criar:  "))
+    tipo_conta = str(input("Tipo da conta que deseja criar: "))
+
+    df = pd.DataFrame()
     
-    if os.path.exists(caminho_excel): # true
+
+    if os.path.exists(caminho_excel): #True
         print("Arquivo ja existe")
         df = pd.read_excel(caminho_excel)
-
-    else: # false
-        print("Arquivo nao existe")
-
-        df = pd.DataFrame()
-
-        # Instancio para manipular os dados adicionados pelo cliente
-        conta = Criar_conta(nome_cliente, cpf, tipo_conta)
-
-        # Identifico o caminho do excel e chamo a funcao salvar_excel
-        novo_dado = conta.salvar_excel(caminho_excel)
-
-        #Concat para inserir uma nova linha no excel com os dados digitados pelo usuario
-        df = pd.concat([df, novo_dado], ignore_index=True)
-
-    df.to_excel(caminho_excel, index=False)
+        #Instaciando o adicionar conta
+        adicionar = Adicionar_conta(nome_cliente, cpf, tipo_conta)
     
+        #Chamando a funcao adicionar que esta dentro da classe Adicionar_conta
+        novo_dado = adicionar.adicionar(df)
+    
+    else: #false
+        print('Arquivo não existe')
+    
+    
+        #Instancio para manipular  os dados adicionados pelo cliente
+        conta = Criar_Conta(nome_cliente, cpf, tipo_conta)
+    
+        #Identifico o caminho do excel e chamo a funcao salvar_excel
+        novo_dado = conta.salvar_excel(caminho_excel)
+    
+    
+        #Concat para inserir uma nova linha no excel com os dados digitados pelo
+    df = pd.concat([df, novo_dado], ignore_index=True)
+    
+    df.to_excel(caminho_excel, index=False)
+
 
 elif opcao == 2:
     print("Opcao 2 selecionada")
+
+    #Validar se o CPF informado e o numero_conta são os mesmos.
+    numero_conta = int(input("Digite o número da conta: "))
+
+    acesso = Acessar_conta(cpf, numero_conta)
+    acesso.validar_banco(caminho_excel)
